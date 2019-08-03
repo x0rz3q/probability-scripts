d72dece584673520def17c96516b02d48f1b87ed
J.M. Thiessen <jacbo@x0rz3q.com>
2019-08-03 10:34:38 +0200

    Add test dataset

--- R/data.r
+++ R/data.r
@@ -1,5 +1,5 @@
 print("Note to set the right dataset in this file!")
 
-data <- read.table('../datasets/datasetA.txt', sep='\t', skip='1')
+data <- read.table('../datasets/test.txt', sep='\t', skip='1')
 ISI <- data$V1
 area <- data$V2
